15b2bf593d7f35a7eb98f7d61f81ae15b608e4c5
jsrsv <jsrsv1234@gmail.com>
2025-11-19 17:51:01 +0530

    AddednewchangestoDashboardhtml

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './Features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+{
+    path: 'admin/dashboard',
+    component:Dashboard
+}
+];
